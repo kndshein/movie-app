a565c2c5193d075832d0a04ee350185d326d4c1a
Kaung Shein <nicholasling1412@gmail.com>
2021-10-11 15:50:03 -0400

    genres fetch and dynamic genres

--- public/movie-app.js
+++ public/movie-app.js
@@ -1,9 +1,6 @@
 let Movie = Backbone.Model.extend({
   urlRoot: "/movies",
   idAttribute: "pk",
-  defaults: {
-    name: "defaultName",
-  },
 });
 
 let Movies = Backbone.Collection.extend({
@@ -14,6 +11,15 @@ let Movies = Backbone.Collection.extend({
 let movies = new Movies();
 
 $(document).ready(function () {
+  let genres = {};
+  fetch("http://localhost:3000/genres")
+    .then((response) => response.json())
+    .then((data) =>
+      data.map((ele) => {
+        genres[ele.pk] = ele.name;
+      })
+    );
+
   let MovieItemView = Backbone.View.extend({
     model: new Movie(),
     tagName: "tr",
@@ -21,7 +27,13 @@ $(document).ready(function () {
       this.template = _.template($(".movie-item-template").html());
     },
     render: function () {
-      this.$el.html(this.template(this.model.toJSON()));
+      this.model = this.model.toJSON();
+      this.model.genres = [];
+      if (this.model.genre_fks)
+        this.model.genre_fks.map((ele) => {
+          this.model.genres.push(genres[ele]);
+        });
+      this.$el.html(this.template(this.model));
       return this;
     },
   });
@@ -32,7 +44,7 @@ $(document).ready(function () {
     initialize: function () {
       this.model.on("add", this.render, this);
       this.model.fetch({
-        // initializes the View by doing an AJAX call. Note that even though nothing is being done to the data in `success` and `error`, the data is stored
+        // initializes the View by doing an AJAX call. Note that even though nothing is being done to the data in `success` and `error` apart from merely console logging , the data is stored
         success: function (response) {
           _.each(response.toJSON(), function (movie) {
             console.log("Successfully GOT movie with _id: " + movie.name);
